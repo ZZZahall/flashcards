ac0c3c1186ebd099683e057370b9be2612ecf7b3
Stella Huayhuaca and Zahall Mahmood <zahall@>
2017-01-10 14:50:27 -0800

    flashcards with toggle

---
+++ flashcards/js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".javascript").click(function() {
+    $(".toggle-class").toggle();
+  });
+  $(".operators").click(function() {
+    $(".toggle2-class").toggle();
+  });
+  $(".variables").click(function() {
+    $(".toggle3-class").toggle();
+  });
+});
